149040518a8a8be3c55b8eeb7217bc5f93bf9498
anufether <anufether@sina.com>
2023-08-18 14:14:27 +0800

    修改测试

--- apps/els.js
+++ apps/els.js
@@ -8,13 +8,13 @@ moment.tz.setDefault('Asia/Shanghai')
 export class RussiaRoundPlatePlugin extends plugin {
   constructor () {
     super({
-      name: '俄罗斯轮盘',
-      dsc: '俄罗斯轮盘',
+      name: '小游戏-俄罗斯轮盘',
+      dsc: '小游戏-俄罗斯轮盘',
       event: 'message',
       priority: 6,
       rule: [
         {
-          reg: '^#?(开启俄罗斯轮盘|开启轮盘|开启转盘|俄罗斯轮盘)$',
+          reg: '^#?(开启俄罗斯轮盘|开始游戏|开启轮盘|开启转盘|俄罗斯轮盘)$',
           fnc: 'start'
         },
         {
@@ -47,14 +47,15 @@ export class RussiaRoundPlatePlugin extends plugin {
 
     // 检查当前时间是否在时间范围内
     if (!currentTime.isBetween(startTime, endTime)) {
-      return e.reply('当前时间段俄罗斯轮盘未开放，请在14:00~17:00时间段准时到来哦~')
+      e.reply('当前俄罗斯轮盘未开放，请在14:00~17:00时间段准时到来哦~')
+      return false
     }
 
     let groupId = e.group_id
     let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
     if (!groupLock) {
       let bulletNum = Math.floor(Math.random() * 5) + 5
-      await redis.get(`HANHAN:ELS:${groupId}`, bulletNum + '', { EX: 10 * 60 * 1000 })
+      await redis.set(`HANHAN:ELS:${groupId}`, bulletNum + '', { EX: 10 * 60 * 1000 })
       await e.reply(`当前群俄罗斯轮盘已开启！\n弹夹有【${bulletNum}】发子弹。\n请发送#开枪 参与游戏`)
     } else {
       await e.reply('当前群俄罗斯轮盘正在进行中！\n请发送#开枪 参与游戏')
@@ -76,7 +77,7 @@ export class RussiaRoundPlatePlugin extends plugin {
 
     // 检查当前时间是否在时间范围内
     if (!currentTime.isBetween(startTime, endTime)) {
-      e.reply('当前俄罗斯轮盘未开放，请在14:00~17:00时间段准时到来哦~')
+      e.reply('当前时间段俄罗斯轮盘未开放，请在14:00~17:00时间段准时到来哦~')
       return false
     }
 
@@ -90,11 +91,11 @@ export class RussiaRoundPlatePlugin extends plugin {
     leftBullets = parseInt(leftBullets)
     if (leftBullets <= 1 || Math.random() < 1 / leftBullets) {
       let group = await Bot.pickGroup(groupId)
-      let max = 30000
-      let min = 6000
+      let max = 300
+      let min = 60
       let time = Math.floor(Math.random() * (max - min + 1)) + min
       await group.muteMember(e.sender.user_id, time)
-      await e.reply(`【${username}】开了一枪，枪响了。\n恭喜【${username}】被禁言${time}秒\n本轮游戏结束。请使用#开启轮盘 开启新一轮游戏`)
+      await e.reply(`【${username}】开了一枪，枪响了。\n恭喜【${username}】被禁言${time}秒\n本轮游戏结束。请使用#开始游戏 开启新一轮游戏`)
       await redis.del(`HANHAN:ELS:${groupId}`)
     } else {
       leftBullets--
@@ -113,7 +114,7 @@ export class RussiaRoundPlatePlugin extends plugin {
     let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
     // e.reply(groupLock)
     if (!groupLock) {
-      e.reply('当前群没有开启轮盘')
+      e.reply('当前群没有开始游戏')
     } else {
       await redis.del(`HANHAN:ELS:${groupId}`)
       e.reply('结束成功')
@@ -128,7 +129,7 @@ export class RussiaRoundPlatePlugin extends plugin {
     let groupId = e.group_id
     let groupLock = await redis.get(`HANHAN:ELS:${groupId}`)
     if (!groupLock) {
-      e.reply('当前群没有开启轮盘')
+      e.reply('当前群没有开始游戏')
     } else {
       e.reply('当前还有' + groupLock + '发子弹')
     }
